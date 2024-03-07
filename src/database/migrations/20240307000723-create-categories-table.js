17a74817e866011c947295282ff136dea3478163
francisco matheus <matheusvale554@gmail.com>
2024-03-06 22:35:20 -0300

    Criando seeders

--- src/database/migrations/20240307000723-create-categories-table.js
+++ src/database/migrations/20240307000723-create-categories-table.js
@@ -2,34 +2,32 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.DataTypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER
       },
       name: {
         allowNull: false,
-        type: Sequelize.DataTypes.STRING,
+        type: Sequelize.DataTypes.STRING
       },
       position: {
         allowNull: false,
         unique: true,
-        type: Sequelize.DataTypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DataTypes.DATE,
+        type: Sequelize.DataTypes.DATE
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DataTypes.DATE,
+        type: Sequelize.DataTypes.DATE
       }
     })
-
-
   },
 
   down: async (queryInterface, Sequelize) => {
