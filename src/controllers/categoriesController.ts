ac775830faed3edd83b23189bf9ae84b82c6633b
francisco matheus <matheusvale554@gmail.com>
2024-03-10 17:02:58 -0300

    obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,19 +1,34 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
+    //GET/ CATEGORIES
     index:async(req:Request,res:Response)=>{
-
+  const [page,perPage] = getPaginationParams(req.query)
+         
         try {
-            const categories = await Category.findAll({
-                attributes:['id','name','position'],
-                order:[['position','ASC']]
-            })
-            return res.json(categories)
-        } catch (error) {
+            const paginatedCategories = await categoryService.findAllPaginated(page,perPage)
+            return res.json(paginatedCategories)
+            }
+            
+         catch (error) {
            if(error instanceof Error){
             return res.status(400).json({message:error.message})
            }
         }
-    }
+    },
+        show: async (req: Request, res: Response) => {
+            const { id } = req.params
+        
+            try {
+              const category = await categoryService.findByIdWithCourses(id)
+              return res.json(category)
+            } catch (err) {
+              if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+              }
+            }
+          }
+    
 }
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,8 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import {User} from './User';
-Category.hasMany(Course)
+
+Category.hasMany(Course,{as:'courses'})
 
 Course.belongsTo(Category)
 
