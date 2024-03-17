8eca048902844d7c9ae2808149e8c09fda40b7e4
francisco matheus <matheusvale554@gmail.com>
2024-03-17 11:42:30 -0300

    obtendo a lista de continuar assistindo

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import {DataTypes, Model,Optional} from "sequelize"
 import { sequelize } from "../database"
 import bcrypt from 'bcrypt'
+import { EpisodeInstance } from "./Episode"
 
 type CheckPasswordCallback = (err?:Error, isSame?:boolean)=>void
 export interface User {
@@ -20,6 +21,7 @@ extends Optional<User,'id'> { }
 
 export interface UserInstance
 extends Model<User, UserCreationAttributes>,User {
+  Episodes?:EpisodeInstance[]
   checkPassword: (password:string,callbackfn:CheckPasswordCallback)=> void
  }
 
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { authController } from "./controllers/authController"
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth"
 import { favoreitesController } from "./controllers/favoritesController"
 import { likesController } from "./controllers/likesController"
+import { usersController } from "./controllers/usersController"
 
 const router = express.Router()
 
@@ -32,4 +33,6 @@ router.delete('/favorites', ensureAuth, favoreitesController.delete)
 
 router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes', ensureAuth, likesController.delete)
+
+router.get('/users/current/watching',ensureAuth,usersController.watching)
 export { router }
