5f26182618a180e998d680f575c00a26e4d0886a
francisco matheus <matheusvale554@gmail.com>
2024-03-18 20:21:58 -0300

    adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import express from "express"
 import { sequelize } from "./database"
 import { adminJs, adminJsRouter } from "./adminjs"
 import { router } from "./routes"
-import { json } from "sequelize"
+import cors from "cors"
+
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.static('public'))
 
